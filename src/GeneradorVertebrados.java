a057f091ab9beece6c5de85bae04e545b8c034a5
Pasovert <pasovert@gmail.com>
2021-09-20 13:24:03 -0500

    agregado gato y pantera

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,7 +4,8 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Dinosaurio");
-
+        miVertebrados[3] = new Vertebrado("Gato", "Pantera");
+ 
         return miVertebrados;
     }
 }
